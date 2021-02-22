6fe2ee31d01080e7ce61ae3e4dd5f47ae11c69ad
migpar <miguelparrapen@gmail.com>
2021-02-22 18:57:38 +0100

    hemos añadido cosas

--- app/src/androidTest/java/com/example/mynicestart/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mynicestart/ExampleInstrumentedTest.java
@@ -2,11 +2,25 @@ package com.example.mynicestart;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+
 
 import static org.junit.Assert.*;
 
@@ -17,10 +31,48 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    /**
+     * PARA ANDROIDX
+     *
+     * @Rule
+     *
+     * public ActivityScenarioRule<LoginActivity> activityRule =
+     *
+     *         new ActivityScenarioRule<>(LoginActivity.class);
+     */
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            LoginActivity.class);
+
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.mynicestart", appContext.getPackageName());
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.mynicestart",
+                appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.buttonLogin)).perform(click());
+        //onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void activityLaunch2() {
+        onView(withId(R.id.buttonRegister)).perform(click());
+        //onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.passwd)).perform(
+                typeText("1234567"));
+        onView(withId(R.id.nickname)).perform(
+                typeText("This is a test."));
+        onView(withId(R.id.buttonLogin)).perform(click());
+       // onView(withId(R.id.text_message)).check(
+         //       matches(withText("This is a test.")));
     }
 }
